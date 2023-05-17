2d58da80e20ab1ac718e940337e1831e558933e1
Radek Włodarski <wlodarski.radek@gmail.com>
2023-05-17 16:27:28 +0200

    introduced color names to theme

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -1,6 +1,23 @@
+const colors = {
+  mineShaft: "#252525", //black text
+  slateGray: "#6E7E91", //text secondary
+  white: "#FFFFFF", //white
+  whiteLilac: "#FBFBFE", //BG light mode
+  scienceBlue: "#0366D6", //Main blue LM
+  anakiwa: "#8CC2FF",
+  mercury: "#E5E5E5", //Light grey
+  dodgerBlue: "#2188FF", //Main blue DM
+  shipCove: "#6D93BE",
+  semiDark: "rgba(54, 54, 54, 0.72)",
+  semiGrey: "rgba(209, 213, 218, 0.1)",
+};
+
 export const theme = {
-  colors: {},
+  boxShadow:
+    "0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03)",
   breakPoints: {
-    phoneMax: "600px",
+    phoneMax: "750px",
   },
+  lightMode: {},
+  darkMode: {},
 };
