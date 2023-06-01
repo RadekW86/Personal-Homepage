79faa53cc41c6d3fc9e6fee47c6b44a8101ccf15
Radek Włodarski <wlodarski.radek@gmail.com>
2023-06-01 14:21:32 +0200

    introduced Sections to HomePage with source data

--- src/features/Homepage/index.js
+++ src/features/Homepage/index.js
@@ -1,14 +1,30 @@
 import { Container } from "../../common/Container/styled";
 import { Main } from "./Main/styled";
+import { Section } from "./Section";
 import { Switch } from "./Switch";
 import { TopSection } from "./TopSection";
+import tools from "../../images/tools.svg";
+import skillset from "../Homepage/skillset.json";
+import rocket from "../../images/rocket.svg";
+import toLearn from "../Homepage/toLearn.json";
 
 export const Homepage = () => {
   return (
     <Container>
       <Switch />
       <TopSection />
-      <Main />
+      <Main>
+        <Section
+          title={"My skillset includes"}
+          emoji={tools}
+          content={skillset.skillset}
+        />
+        <Section
+          title={"What I want to learn next"}
+          emoji={rocket}
+          content={toLearn.toLearn}
+        />
+      </Main>
     </Container>
   );
 };
