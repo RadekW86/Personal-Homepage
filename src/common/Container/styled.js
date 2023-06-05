e35ab27adaee6dc418ca16e8680284be425f8be2
Radek Włodarski <wlodarski.radek@gmail.com>
2023-06-05 16:40:35 +0200

    improved container display

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1230px;
+  max-width: 1260px;
   margin: 0 auto;
   margin-top: 90px;
   margin-bottom: 110px;
+  padding: 0 30px;
 
   @media (max-width: ${({ theme }) => theme.breakPoints.phoneMax}) {
-    max-width: 275px;
+    max-width: 295px;
     margin-top: 8px;
     margin-bottom: 31px;
+    padding: 0 20px;
   }
 `;
