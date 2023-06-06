74971cee30375939a90c21c9a056dd5839d21bf9
Radek Włodarski <wlodarski.radek@gmail.com>
2023-06-06 12:41:42 +0200

    created a Slice for portfolio data

--- src/core/store.js
+++ src/core/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homepageReducers from "../features/Homepage/homepageSlice";
+import reposReducers from "../features/Homepage/reposSlice";
 
 const store = configureStore({
   reducer: {
     homepage: homepageReducers,
+    portfolio: reposReducers,
   },
 });
 
