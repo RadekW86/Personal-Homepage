6529968c009870dab57c844de4654b588d2525ca
Radek Włodarski <wlodarski.radek@gmail.com>
2023-06-07 21:08:31 +0200

    Updates

--- src/core/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import { watchFetchRepos } from "../features/Homepage/reposSaga";
-
-export default function* () {
-  yield all([watchFetchRepos()]);
-}
--- src/features/Homepage/routes.js
+++
@@ -1,6 +0,0 @@
-export const contact = "wlodarski.radek@gmail.com";
-export const repositories = "https://api.github.com/users/RadekW86/repos";
-export const gitHub = "https://github.com/RadekW86";
-export const facebook = "";
-export const linkedIn = "https://www.linkedin.com/in/radoslaw-wlodarski/";
-export const instagram = "https://www.instagram.com/radek_w86/";
